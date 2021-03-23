ba873b1a98c3787a1f8795487666a99cdada137a
cyblogerz <pranavajay2002@gmail.com>
2021-03-23 19:41:37 +0530

    Added custom font

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,89 @@ void main() {
 
          body: SafeArea(
            child:Column(
+             mainAxisAlignment: MainAxisAlignment.center,
              children: <Widget> [
-               CircleAvatar(
-                 backgroundImage: AssetImage('images/profile.jpeg'),
-                 backgroundColor: Colors.green,
-                 radius: 50.0,
+               Center(
+                 child: CircleAvatar(
+                   backgroundImage: AssetImage('images/profile.jpeg'),
+                   backgroundColor: Colors.green,
+                   radius: 50.0,
+                 ),
                ),
-               Text(
-                 'Pranav Ajay',
-                 style: TextStyle(
-                   fontWeight: FontWeight.bold,
-                   fontSize: 40.0,
-                   color: Colors.white,
+               Center(
+                 child: Text(
+                   'Pranav Ajay',
+                   style: TextStyle(
+                     fontFamily: 'Pacifico',
+                     fontWeight: FontWeight.bold,
+                     fontSize: 40.0,
+                     color: Colors.white,
+
+                   ),
+
 
                  ),
+               ),
+               Center(
+                 child: Text(
+                   'FLUTTER DEVELOPER',
+                   style: TextStyle(
+                     fontFamily: 'Source Sans Pro',
+                     fontSize: 20.0,
+                     fontWeight: FontWeight.bold,
+                     color:Colors.teal[100],
+                     letterSpacing: 2.5,
 
+                   ),
+
+                 ),
+               ),
+               SizedBox(
+                 height:20.0,
+                 width: 150,
+                 child: Divider(
+                   color: Colors.teal[100],
+                 ),
+               ),
+
+
+
+               Card(
+                 color: Colors.white,
+                 margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                 child: ListTile(
+                   leading:Icon(
+                     Icons.phone,
+                     color:Colors.teal[900],
+                   ),
+                   title: Text(
+                     '+91 8921335035',
+                     style: TextStyle(
+                       fontFamily: 'Source Sans Pro',
+                       fontSize: 20.0,
+                       color: Colors.teal[900],
+
+                     ),
+                   ),
+                 ),
+               ),
+               Card(
+                 color:Colors.white,
+                 margin:EdgeInsets.symmetric(vertical:10.0,horizontal: 25.0),
+                 child: ListTile(
+                   leading: Icon(
+                     Icons.email,
+                     color: Colors.teal[900],
+                   ),
+                   title: Text(
+                     'pranavajay2002@gmail.com',
+                     style: TextStyle(
+                       fontFamily: 'Source Sans Pro',
+                       fontSize:18.0,
+                       color: Colors.teal[900],
+                     ),
+                   )
+                 ),
 
                ),
 
@@ -35,6 +103,7 @@ void main() {
 
              ]
            ),
+
 
          ),
        ),
